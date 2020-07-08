6f0e766fb49869e4df232c6f5a428eb9e2fd024d
Chatchai <dchatchai@gmail.com>
2020-07-08 11:05:56 +0700

    Update Shop Info

---
+++ lib/page/add_info_shop.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:onerc/utility/my_style.dart';
+
+class AddInfoShop extends StatefulWidget {
+  @override
+  _AddInfoShopState createState() => _AddInfoShopState();
+}
+
+class _AddInfoShopState extends State<AddInfoShop> {
+  String dateTimeString;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    findCurrentTime();
+  }
+
+  Future<Null> findCurrentTime() async {
+    DateTime dateTime = DateTime.now();
+    setState(() {
+      dateTimeString = DateFormat('dd-MM-yyyy').format(dateTime);
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('เพิ่มข้อมูลร้านค้า'),
+      ),
+      body: Column(
+        children: <Widget>[
+          MyStyle().showTextH2('วันเวลาที่บันทึก : '),
+          MyStyle().showTextH1(dateTimeString == null ? '' : dateTimeString)
+        ],
+      ),
+    );
+  }
+}
