ac190d16c922dce30dd29c4eb6a7afcb516dabd5
Chatchai <dchatchai@gmail.com>
2020-07-09 11:20:11 +0700

    Photo

--- lib/widget/show_my_product.dart
+++ lib/widget/show_my_product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:onerc/page/add_product_shop.dart';
 
 class ShowMyProduct extends StatefulWidget {
   @override
@@ -8,6 +9,17 @@ class ShowMyProduct extends StatefulWidget {
 class _ShowMyProductState extends State<ShowMyProduct> {
   @override
   Widget build(BuildContext context) {
-    return Text('This is My Products');
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          MaterialPageRoute route = MaterialPageRoute(
+            builder: (context) => AddProductShop(),
+          );
+          Navigator.push(context, route).then((value) => null);
+        },
+        child: Icon(Icons.restaurant_menu),
+      ),
+      body: Text('This is My Products123'),
+    );
   }
 }
